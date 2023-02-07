1081d08c8c84b880af26fcf361ac1c2b69d7be2f
Ashura <116311276+Ashura667@users.noreply.github.com>
2023-02-07 10:38:06 +0100

    Ajout d'un nouveau composant pour la notation.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import logo from "./images/icon-star.svg";
+import Notation_scale from "./Notation_scale";
 function App() {
   return (
     <div className="card_rating">
@@ -16,7 +17,9 @@ function App() {
           Please let us know how we did with your support request. All feedback is appreciated to help us
 improve our offering!
           </div>
-          
+          <div className="notation">
+            <Notation_scale />
+            </div>
         </div>
     </div>
   );
---
+++ src/Notation_scale.js
@@ -0,0 +1,8 @@
+import "./Notation_scale.css";
+function Notation_scale() {
+  return (<div>
+    {() => {return (<div>fdsfsd</div>)}}
+  </div>);
+}
+
+export default Notation_scale;
