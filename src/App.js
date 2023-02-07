1c390f65c436fd9caa97f283e28245febf1fec96
Ashura <116311276+Ashura667@users.noreply.github.com>
2023-02-07 10:11:21 +0100

    Modification du background et ajustement de l'étoile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import logo from './images/icon-star.svg'
 function App() {
   return (
     <div className='card_rating'>
-        <div className=''>
+        <div className='background_card'>
+          
+          <div className='background_logo'>
+          <img className='logo_star' src={logo} alt="fdsfsd"/>
 
+          </div>
         </div>
     </div>
   );
